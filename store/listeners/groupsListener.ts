9f0e644707c4176e849c5cf904cbb12451783521
admin <acceldevco@gmail.com>
2025-11-24 14:21:15 +0330

    store/listeners/groupsListene

--- store/listeners/groupsListener.ts
+++ store/listeners/groupsListener.ts
@@ -1,15 +1,15 @@
-// src/store/listeners/groupsListener.ts
-import { fetchGroups, handleGroup } from "@/app/features/group/groupSlice";
-import { createListenerMiddleware } from "@reduxjs/toolkit";
-// import { handleGroup, fetchGroups } from "";
+// // src/store/listeners/groupsListener.ts
+// import { fetchGroups, handleGroup } from "@/app/features/group/groupSlice";
+// import { createListenerMiddleware } from "@reduxjs/toolkit";
+// // import { handleGroup, fetchGroups } from "";
 
-const groupsListener = createListenerMiddleware();
+// const groupsListener = createListenerMiddleware();
 
-groupsListener.startListening({
-  actionCreator: handleGroup.fulfilled,
-  effect: async (action, listenerApi) => {
-    await listenerApi.dispatch(fetchGroups());
-  },
-});
+// groupsListener.startListening({
+//   actionCreator: handleGroup.fulfilled,
+//   effect: async (action, listenerApi) => {
+//     await listenerApi.dispatch(fetchGroups());
+//   },
+// });
 
-export default groupsListener;
+// export default groupsListener;
--- store/listeners/tasksListener.ts
+++ store/listeners/tasksListener.ts
@@ -1,18 +1,18 @@
-// src/store/listeners/groupsListener.ts
-import { handleGroup } from "@/app/features/group/groupSlice";
-import { fetchTasks, handleTask } from "@/app/features/tasks/taskSlice";
-import { createListenerMiddleware } from "@reduxjs/toolkit";
-// import { handleGroup, fetchGroups } from "";
+// // src/store/listeners/groupsListener.ts
+// import { handleGroup } from "@/app/features/group/groupSlice";
+// import { fetchTasks, handleTask } from "@/app/features/tasks/taskSlice";
+// import { createListenerMiddleware } from "@reduxjs/toolkit";
+// // import { handleGroup, fetchGroups } from "";
 
-const tasksListener = createListenerMiddleware();
+// const tasksListener = createListenerMiddleware();
 
-tasksListener.startListening({
-  actionCreator: handleTask.fulfilled,
-  effect: async (action, listenerApi) => {
-    await listenerApi.dispatch(fetchTasks({}));
-  },
-});
+// tasksListener.startListening({
+//   actionCreator: handleTask.fulfilled,
+//   effect: async (action, listenerApi) => {
+//     await listenerApi.dispatch(fetchTasks({}));
+//   },
+// });
 
-export default tasksListener;
+// export default tasksListener;
 
-/////////////////////////////////
+// /////////////////////////////////
